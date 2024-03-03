c0742dc94d29355bc6d6c74ce9b696b03b233b79
XiaoqingShen1 <162057042+XiaoqingShen1@users.noreply.github.com>
2024-03-03 22:09:52 +0000

    Add files via upload

---
+++ Passenger.java
@@ -0,0 +1,45 @@
+public class Passenger {
+    private String bookingReference;
+    private String name;
+    private String flightCode;
+    private boolean checkedIn;
+    private double baggageWeight;
+    private double baggageVolume;
+
+//    // Constructor
+//    public Passenger(String bookingReference, String name, String flightCode) {
+//        this.bookingReference = bookingReference;
+//        this.name = name;
+//        this.flightCode = flightCode;
+//        this.checkedIn = false; // Default value
+//    }
+//
+//    // Check-in method
+//    public void checkIn(double baggageWeight, double baggageVolume) {
+//        this.checkedIn = true;
+//        this.baggageWeight = baggageWeight;
+//        this.baggageVolume = baggageVolume;
+//        // Here you could calculate and set any excess baggage fees, if necessary
+//    }
+//
+//    // Getter and Setter methods
+//    // For example:
+//    public String getName() {
+//        return name;
+//    }
+//
+//    // Include other getters and setters as needed
+//
+//    // You might also want to override the toString() method for easier debugging
+//    @Override
+//    public String toString() {
+//        return "Passenger{" +
+//               "bookingReference='" + bookingReference + '\'' +
+//               ", name='" + name + '\'' +
+//               ", flightCode='" + flightCode + '\'' +
+//               ", checkedIn=" + checkedIn +
+//               ", baggageWeight=" + baggageWeight +
+//               ", baggageVolume=" + baggageVolume +
+//               '}';
+//    }
+//}
